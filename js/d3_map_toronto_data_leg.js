c0c40deafe5dae92bb33d8ee2214fb33ca6c56e1
amandakeasson <easson.amanda@gmail.com>
2019-02-02 22:58:43 -0500

    update style

--- js/d3_map_toronto_data_leg.js
+++ js/d3_map_toronto_data_leg.js
@@ -1,21 +1,13 @@
-// based off of https://github.com/jasonicarter/toronto-geojson
+// references:
+// toronto map: https://github.com/jasonicarter/toronto-geojson
 // merging with csv data: http://bl.ocks.org/michellechandra/0b2ce4923dc9b5809922
 // also help from http://bl.ocks.org/feyderm/e6cab5931755897c2eb377ccbf9fdf18
 // linspace function: https://gist.github.com/davebiagioni/1ac21feb1c2db04be4e6
 // legend help: https://bl.ocks.org/starcalibre/6cccfa843ed254aa0a0d
 
-// data scoure: City of Toronto
-
-var pageTitle = d3.select(".titleFormat")
-  .text("Toronto Maps")
-  .style("margin-left", "10px")
-  .style("margin-top", "0px")
-  .style("font-family", "Calibri")
-  .style("font-size", "40px");
-
 d3.csv("WB-Economics.csv", function(data) {
 
-  updateGraph('Businesses'); // initial data
+  updateGraph('Businesses');
 
   var datacols = d3.values(data)[0];
   var datavars = Object.keys(datacols)
@@ -30,7 +22,7 @@ d3.csv("WB-Economics.csv", function(data) {
 
   var dropdown = d3.select("#ddtest")
       .style("margin-left", "10px")
-      .style("margin-top", "10px")
+      .style("margin-top", "0px")
       .style("font-family", "Calibri")
       .style("font-size", "14px")
       .on("change", dropdownChange)
@@ -48,16 +40,15 @@ d3.csv("WB-Economics.csv", function(data) {
     d3.json("toronto_topo.json", function(topology) {
 
       d3.selectAll("svg").remove()
-      //d3.selectAll('#legend-svg').remove()
 
       var neighbourhoods = topojson.feature(topology, topology.objects.toronto); // topojson.feature works for v1, not v0
 
       for (var i = 0; i < data.length; i++) {
         dataValue = eval('data[' + i.toString() + '].' + data_selected) // convert data point from str to int
         dataValue = +dataValue
-        var dataID = data[i].id; // neighbourhood ID
+        var dataID = data[i].id;
         for (var j = 0; j < neighbourhoods.features.length; j++)  {
-          var jsonID = neighbourhoods.features[j].properties.id; // find corresponding ID
+          var jsonID = neighbourhoods.features[j].properties.id;
           if (dataID == jsonID) {
             neighbourhoods.features[j].properties.valtoplot = dataValue;
           break;
@@ -77,7 +68,7 @@ d3.csv("WB-Economics.csv", function(data) {
         //.interpolate(d3.interpolateHcl)
         //.range(["blue", "pink"]); // [d3.rgb('#007AFF'), d3.rgb('#FFF500')]
 
-      var width = 700, height = 425;
+      var width = 700, height = 400;
 
       var projection = d3.geoAlbers()
           .scale(1)
@@ -122,24 +113,19 @@ d3.csv("WB-Economics.csv", function(data) {
         .on("mouseover", mouseover)
         .on("mouseout", mouseout)
 
-      var legendFullHeight = .7*height;
-      var legendFullWidth = 80;
+      var fullHeight = .7*height;
+      var fullWidth = 80;
 
-      var legendMargin = { top: 10, bottom: 20, left: 5, right: 60 };
+      var legMargin = { top: 10, bottom: 20, left: 5, right: 60 };
+      var legWidth = fullWidth - legMargin.right;
+      var legHeight = fullHeight - legMargin.top - legMargin.bottom;
 
-      // use same margins as main plot
-      var legendWidth = legendFullWidth - legendMargin.right;
-      var legendHeight = legendFullHeight - legendMargin.top - legendMargin.bottom;
-
-      var legendSvg = d3.select("#main-svg")
+      var legSvg = d3.select("#main-svg")
           .append("svg")
-          .attr('width', legendFullWidth)
-          .attr('height', legendFullHeight)
-          //.append('g')
-          //.attr('transform', 'translate(' + legendMargin.left + ',' +
-          //legendMargin.top + ')');
+          .attr('width', fullWidth)
+          .attr('height', fullHeight)
 
-      var gradient = legendSvg.append("defs")
+      var gradient = legSvg.append("defs")
             .append('svg:linearGradient')
             .attr('id', 'gradient')
             .attr('x1', '0%') // bottom
@@ -148,9 +134,6 @@ d3.csv("WB-Economics.csv", function(data) {
             .attr('y2', '0%')
             .attr('spreadMethod', 'pad');
 
-      // programatically generate the gradient for the legend
-      // this creates an array of [pct, colour] pairs as stop
-      // values for legend
       var linspace = function(start, stop, nsteps){
         delta = (stop-start)/(nsteps-1)
         return d3.range(start, stop+delta, delta).slice(0, nsteps)
@@ -169,29 +152,24 @@ d3.csv("WB-Economics.csv", function(data) {
             .attr('stop-opacity', 1);
           };
 
-      legendSvg.append('rect')
+      legSvg.append('rect')
           .attr('x1', 0)
           .attr('y1', 0)
-          .attr('width', legendWidth)
-          .attr('height', legendHeight)
+          .attr('width', legWidth)
+          .attr('height', legHeight)
           .style('fill', 'url(#gradient)');
 
-      // create a scale and axis for the legend
-      var legendScale = d3.scaleLinear()
+      var legScale = d3.scaleLinear()
           .domain([crange[0], crange[1]])
-          .range([legendHeight, 0]);
+          .range([legHeight, 0]);
 
-      var legendAxis = d3.axisRight()
-          .scale(legendScale);
-          //.tickFormat(d3.format("d"));
+      var legAxis = d3.axisRight()
+          .scale(legScale);
 
-      legendSvg.append("g")
+      legSvg.append("g")
           .attr("class", "legend axis")
-          .attr("transform", "translate(" + legendWidth + ", 0)")
-          .call(legendAxis);
-
-      // end legend
-
+          .attr("transform", "translate(" + legWidth + ", 0)")
+          .call(legAxis);
 
       function updateFill(selection, data_selected, color) {
         selection.transition().duration(0)
